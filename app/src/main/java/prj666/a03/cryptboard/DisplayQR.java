8029a1ae1d526751a386b1c18239be98d9a52fb3
dcoull <dillon.coull@gmail.com>
2018-09-14 16:45:50 -0400

    DisplayQR layout creation

---
+++ app/src/main/java/prj666/a03/cryptboard/DisplayQR.java
@@ -0,0 +1,13 @@
+package prj666.a03.cryptboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_qr);
+    }
+}
