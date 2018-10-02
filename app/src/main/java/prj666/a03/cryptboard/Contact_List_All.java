d6633b26293b9c89225f85137b5eb4174b8e84e3
alexlp97 <alelatorraca97@gmail.com>
2018-10-02 17:17:01 -0400

    Adding contact list

---
+++ app/src/main/java/prj666/a03/cryptboard/Contact_List_All.java
@@ -0,0 +1,35 @@
+package prj666.a03.cryptboard;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+public class Contact_List_All extends ListFragment implements AdapterView.OnItemClickListener {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contact__list__main, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.all, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+}
--- app/src/main/java/prj666/a03/cryptboard/MainActivity.java
+++ app/src/main/java/prj666/a03/cryptboard/MainActivity.java
@@ -1,7 +1,10 @@
 package prj666.a03.cryptboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        Button con = findViewById(R.id.Contacts);
+
+        con.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Contact_List_Main.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     /**
