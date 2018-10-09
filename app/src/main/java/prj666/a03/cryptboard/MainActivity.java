f21bb5ecbfdbdb46c5059c44533d451e4c993312
dcoull <dcoull@myseneca.ca>
2018-10-09 16:12:14 -0400

    Merging master with key exchange

--- app/src/main/java/prj666/a03/cryptboard/MainActivity.java
+++ app/src/main/java/prj666/a03/cryptboard/MainActivity.java
@@ -1,8 +1,7 @@
 package prj666.a03.cryptboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
-}
+    //public native String stringFromJNI();
+
+
+
+    }
